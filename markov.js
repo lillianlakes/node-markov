60b4cc480ce650b5e6579edce6412e44609cd926
lillianlakes <lillian.lakes@gmail.com>
2021-07-27 15:29:31 -0700

    completes makeChains and part of getText

--- markov.js
+++ markov.js
@@ -6,8 +6,13 @@ class MarkovMachine {
   /** build markov machine; read in text.*/
 
   constructor(text) {
+      /* splits text on one or more spaces, carriage returns or new 
+      lines using RegEx, and puts them in an array */
     let words = text.split(/[ \r\n]+/);
     // MORE CODE HERE
+    this.words = words;
+    this.chainsObj = this.makeChains();
+
   }
 
   /** set markov chains:
@@ -17,6 +22,18 @@ class MarkovMachine {
 
   makeChains() {
     // MORE CODE HERE
+    const chains = {};
+    for (let i = 0; i < this.words.length; i++) {
+        if (!(this.words[i] in chains)) {
+            chains[this.words[i]] = [];        
+        }
+        if (this.words[i + 1] === undefined) {
+            chains[this.words[i]].push(null);
+        } else {
+            chains[this.words[i]].push(this.words[i + 1]);
+        }
+    }
+    return chains;
   }
 
 
@@ -24,5 +41,19 @@ class MarkovMachine {
 
   getText(numWords = 100) {
     // MORE CODE HERE
+
+    let randomStartingWordIndex = Math.floor((Math.random() * this.words.length));
+
+    let randomStartingWord = this.words[randomStartingWordIndex];
+
+    let nextWords = this.chainsObj[randomStartingWord];
+
+    let randomNextWordIndex = Math.floor((Math.random() * nextWords.length));
+
+    let randomNextWord = this.chainsObj[randomStartingWord][randomNextWordIndex];
   }
 }
+
+let mm = new MarkovMachine("the cat in the hat");
+
+// console.log(mm.makeChains())
